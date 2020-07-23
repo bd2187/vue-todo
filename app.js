fa7aa877eced92313805ac306e48dfa1bd1ff6c5
bd2187 <brandon.dionisio.26@gmail.com>
2020-07-23 08:15:56 -0700

    add addTodo() and validateNewTodo()

--- app.js
+++ app.js
@@ -11,9 +11,32 @@ Vue.component("todo", {
 var app = new Vue({
     el: "#app",
     data: {
+        newTodo: "",
         todos: [
             { id: 1, message: "wash car", active: true },
             { id: 2, message: "canyon drive", active: false },
         ],
+
+        /**
+         * Checks if new todo is valid
+         * @param String todo
+         * @return Boolean
+         */
+        validateNewTodo: function (todo) {
+            return todo.trim() !== "" ? true : false;
+        },
+    },
+
+    methods: {
+        /**
+         * Adds todo to todos array if valid
+         * @param Object evt
+         * @return
+         */
+        addTodo: function (evt) {
+            evt.preventDefault();
+            var isTodoValid = this.validateNewTodo(this.newTodo);
+            console.warn(isTodoValid);
+        },
     },
 });
